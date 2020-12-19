b78bc8933e2ef105b15e2c1e500b4f586f23896d
willi552 <37137412+willi552@users.noreply.github.com>
2020-12-18 17:35:06 -0800

    added selector for method to retrieve serial number also i guess it looks better now

--- app/src/main/java/ws/vika/deviceinfo/MainActivity.java
+++ app/src/main/java/ws/vika/deviceinfo/MainActivity.java
@@ -8,8 +8,8 @@ import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -22,10 +22,11 @@ import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText;
+    TextView textView;
     Button convertTextButton;
     Button getSerialNumberButton;
     Button getImeiNumberButton;
+    Button b1, b2, b3, b4, b5;
     ImageView imageView;
 
     public static final int REQUEST_READ_PHONE_STATE = 0;
@@ -35,11 +36,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = findViewById(R.id.convertTextField);
-        convertTextButton = findViewById(R.id.convertButton);
+        textView = findViewById(R.id.appOutput);
         getSerialNumberButton = findViewById(R.id.getSerialNumberButton);
         getImeiNumberButton = findViewById(R.id.getImeiButton);
         imageView = findViewById(R.id.barcodeOutput);
+
+        b1 = findViewById(R.id.methodOneButton);
+        b2 = findViewById(R.id.methodTwoButton);
+        b3 = findViewById(R.id.methodThreeButton);
+        b4 = findViewById(R.id.methodFourButton);
+        b5 = findViewById(R.id.methodFiveButton);
 
         int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
 
@@ -49,22 +55,12 @@ public class MainActivity extends AppCompatActivity {
             //TODO
         }
 
-        convertTextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Bitmap bitmap = generateBarcode(editText.getText().toString(),
-                        BarcodeFormat.CODE_128,
-                        imageView.getWidth(),
-                        imageView.getHeight());
-                imageView.setImageBitmap(bitmap);
-            }
-        });
 
         getSerialNumberButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editText.setText(getSerialNumber());
-                Bitmap bitmap = generateBarcode(editText.getText().toString(),
+                textView.setText(getSerialNumber());
+                Bitmap bitmap = generateBarcode(textView.getText().toString(),
                         BarcodeFormat.CODE_128,
                         imageView.getWidth(),
                         imageView.getHeight());
@@ -75,8 +71,68 @@ public class MainActivity extends AppCompatActivity {
         getImeiNumberButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editText.setText(getImeiNumber());
-                Bitmap bitmap = generateBarcode(editText.getText().toString(),
+                textView.setText(getImeiNumber());
+                Bitmap bitmap = generateBarcode(textView.getText().toString(),
+                        BarcodeFormat.CODE_128,
+                        imageView.getWidth(),
+                        imageView.getHeight());
+                imageView.setImageBitmap(bitmap);
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(getSerialNumber(1));
+                Bitmap bitmap = generateBarcode(textView.getText().toString(),
+                        BarcodeFormat.CODE_128,
+                        imageView.getWidth(),
+                        imageView.getHeight());
+                imageView.setImageBitmap(bitmap);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(getSerialNumber(2));
+                Bitmap bitmap = generateBarcode(textView.getText().toString(),
+                        BarcodeFormat.CODE_128,
+                        imageView.getWidth(),
+                        imageView.getHeight());
+                imageView.setImageBitmap(bitmap);
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(getSerialNumber(3));
+                Bitmap bitmap = generateBarcode(textView.getText().toString(),
+                        BarcodeFormat.CODE_128,
+                        imageView.getWidth(),
+                        imageView.getHeight());
+                imageView.setImageBitmap(bitmap);
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(getSerialNumber(4));
+                Bitmap bitmap = generateBarcode(textView.getText().toString(),
+                        BarcodeFormat.CODE_128,
+                        imageView.getWidth(),
+                        imageView.getHeight());
+                imageView.setImageBitmap(bitmap);
+            }
+        });
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(getSerialNumber(5));
+                Bitmap bitmap = generateBarcode(textView.getText().toString(),
                         BarcodeFormat.CODE_128,
                         imageView.getWidth(),
                         imageView.getHeight());
@@ -147,6 +203,69 @@ public class MainActivity extends AppCompatActivity {
             if (serialNumber.equals(Build.UNKNOWN)) {
                 serialNumber = null;
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+            serialNumber = null;
+        }
+
+        return serialNumber;
+    }
+
+    // below method from https://gist.github.com/flawyte/efd23dd520fc2320f94ba003b9aabfce
+    private static String getSerialNumber(int i) {
+
+        String serialNumber = "nothing";
+
+        try {
+
+            Class<?> c = Class.forName("android.os.SystemProperties");
+            Method get = c.getMethod("get", String.class);
+
+            if (i == 1) {
+                // (?) Lenovo Tab (https://stackoverflow.com/a/34819027/1276306)
+                serialNumber = (String) get.invoke(c, "gsm.sn1");
+            }
+
+            if (i == 2) {
+                // Samsung Galaxy S5 (SM-G900F) : 6.0.1
+                // Samsung Galaxy S6 (SM-G920F) : 7.0
+                // Samsung Galaxy Tab 4 (SM-T530) : 5.0.2
+                // (?) Samsung Galaxy Tab 2 (https://gist.github.com/jgold6/f46b1c049a1ee94fdb52)
+                serialNumber = (String) get.invoke(c, "ril.serialnumber");
+            }
+
+            if (i == 3) {
+                // Archos 133 Oxygen : 6.0.1
+                // Google Nexus 5 : 6.0.1
+                // Hannspree HANNSPAD 13.3" TITAN 2 (HSG1351) : 5.1.1
+                // Honor 5C (NEM-L51) : 7.0
+                // Honor 5X (KIW-L21) : 6.0.1
+                // Huawei M2 (M2-801w) : 5.1.1
+                // (?) HTC Nexus One : 2.3.4 (https://gist.github.com/tetsu-koba/992373)
+                serialNumber = (String) get.invoke(c, "ro.serialno");
+            }
+
+            if (i == 4) {
+                // Samsung Galaxy S5 (SM-G900F) : 6.0.1
+                // Samsung Galaxy S6 (SM-G920F) : 7.0
+                // Samsung Galaxy Tab 4 (SM-T530) : 5.0.2
+                // (?) Samsung Galaxy Tab 2 (https://gist.github.com/jgold6/f46b1c049a1ee94fdb52)
+                serialNumber = (String) get.invoke(c, "ril.serialnumber");
+            }
+
+            if (i == 5) {
+                // (?) Samsung Galaxy Tab 3 (https://stackoverflow.com/a/27274950/1276306)
+                serialNumber = (String) get.invoke(c, "sys.serialnumber");
+            }
+
+            if (i == 6) {
+                // Archos 133 Oxygen : 6.0.1
+                // Hannspree HANNSPAD 13.3" TITAN 2 (HSG1351) : 5.1.1
+                // Honor 9 Lite (LLD-L31) : 8.0
+                // Xiaomi Mi 8 (M1803E1A) : 8.1.0
+                serialNumber = Build.SERIAL;
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
             serialNumber = null;
